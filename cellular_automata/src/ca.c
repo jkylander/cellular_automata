744115ac9ccf00ea510daf624ebf8f4900e77988
Julian Kylander <julian.kylander@gmail.com>
2024-01-16 22:17:47 +0100

    updating without moving down

--- cellular_automata/src/ca.c
+++ cellular_automata/src/ca.c
@@ -1,14 +1,19 @@
 #include <raylib.h>
+
 #define CELL_WIDTH 16
 #define CELL_LENGTH 10
-#define SCREEN_WIDTH 410
-#define SCREEN_HEIGHT 410
-#define TOTAL SCREEN_WIDTH/CELL_WIDTH
-int cells[TOTAL];
-int nextCells[TOTAL];
+#define SCREEN_WIDTH 810
+#define SCREEN_HEIGHT 810
+
+#define TOTAL_CELLS SCREEN_WIDTH / CELL_LENGTH
+
+int cells[TOTAL_CELLS];
+int nextCells[TOTAL_CELLS];
+
 void setup(void) {
-    cells[CELL_LENGTH / 2 ] = 1;
+    cells[TOTAL_CELLS / 2 ] = 1;
 }
+
 // Update cell state
 int calculate_state(int a, int b, int c) {
     if (a == 1 && b == 1 && c == 1) return 1;
@@ -24,7 +29,7 @@ int calculate_state(int a, int b, int c) {
 void drawCells(int y) {
 
     int x = 0;
-    for (int i = 0; i < CELL_LENGTH; i++) {
+    for (int i = 0; i < TOTAL_CELLS; i++) {
         x = i * CELL_WIDTH;
         // Draw cells, 1 is black, 0 is white
         DrawRectangle(
@@ -40,15 +45,15 @@ void drawCells(int y) {
 void updateCells(void) {
 
     nextCells[0] = cells[0];
-    nextCells[TOTAL- 1] = cells[TOTAL - 1];
-    for (int i = 1; i < CELL_LENGTH - 1; i++) {
+    nextCells[TOTAL_CELLS - 1] = cells[TOTAL_CELLS - 1];
+    for (int i = 1; i < TOTAL_CELLS - 1; i++) {
         int left = cells[i - 1];
         int right = cells[i + 1];
         int state = cells[i];
         int newState = calculate_state(left, state, right);
         nextCells[i] = newState;
     }
-    for (int i = 0; i < CELL_WIDTH; i++){
+    for (int i = 0; i < TOTAL_CELLS; i++){
         cells[i] = nextCells[i];
     }
 }
@@ -56,8 +61,10 @@ int main(void) {
     // Window loop
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Cellular Automata");
     int y = 0;
+    setup();
     while(!WindowShouldClose())
     {
+        ClearBackground(RAYWHITE);
         BeginDrawing();
             drawCells(y);
             updateCells();

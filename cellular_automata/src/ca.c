ba237703501cb94d7199a13c457dd357b3c3a47a
Julian Kylander <julian.kylander@gmail.com>
2024-01-16 14:02:52 +0100

    initial

---
+++ cellular_automata/src/ca.c
@@ -0,0 +1,69 @@
+#include <raylib.h>
+#define CELL_WIDTH 16
+#define CELL_LENGTH 10
+#define SCREEN_WIDTH 410
+#define SCREEN_HEIGHT 410
+#define TOTAL SCREEN_WIDTH/CELL_WIDTH
+int cells[TOTAL];
+int nextCells[TOTAL];
+void setup(void) {
+    cells[CELL_LENGTH / 2 ] = 1;
+}
+// Update cell state
+int calculate_state(int a, int b, int c) {
+    if (a == 1 && b == 1 && c == 1) return 1;
+    if (a == 1 && b == 1 && c == 0) return 0;
+    if (a == 1 && b == 0 && c == 1) return 1;
+    if (a == 1 && b == 0 && c == 0) return 1;
+    if (a == 0 && b == 1 && c == 1) return 0;
+    if (a == 0 && b == 1 && c == 0) return 1;
+    if (a == 0 && b == 0 && c == 1) return 1;
+    if (a == 0 && b == 0 && c == 0) return 0;
+    return 0;
+}
+void drawCells(int y) {
+
+    int x = 0;
+    for (int i = 0; i < CELL_LENGTH; i++) {
+        x = i * CELL_WIDTH;
+        // Draw cells, 1 is black, 0 is white
+        DrawRectangle(
+            x,
+            y,
+            CELL_WIDTH,
+            CELL_WIDTH,
+            (Color) {255 - 255*cells[i], 255 -255*cells[i], 255 - 255*cells[i], 255}
+        );
+        DrawRectangleLines(i * CELL_WIDTH, 0, CELL_WIDTH, CELL_WIDTH, BLACK);
+    }
+}
+void updateCells(void) {
+
+    nextCells[0] = cells[0];
+    nextCells[TOTAL- 1] = cells[TOTAL - 1];
+    for (int i = 1; i < CELL_LENGTH - 1; i++) {
+        int left = cells[i - 1];
+        int right = cells[i + 1];
+        int state = cells[i];
+        int newState = calculate_state(left, state, right);
+        nextCells[i] = newState;
+    }
+    for (int i = 0; i < CELL_WIDTH; i++){
+        cells[i] = nextCells[i];
+    }
+}
+int main(void) {
+    // Window loop
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Cellular Automata");
+    int y = 0;
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+            drawCells(y);
+            updateCells();
+            // Move down 1 row
+            //y += CELL_WIDTH;
+        EndDrawing();
+    }
+    return 0;
+}

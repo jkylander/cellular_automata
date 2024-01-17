670d03b95ac644dc3f9cbf6151d05263d25cba0a
Julian Kylander <julian.kylander@gmail.com>
2024-01-17 13:38:58 +0100

    removed un-neccesary arguments

--- cellular_automata/src/ca.c
+++ cellular_automata/src/ca.c
@@ -12,7 +12,6 @@ const uint8_t rule_value = 110;
 
 int cells[TOTAL_CELLS];
 int nextCells[TOTAL_CELLS];
-void reverse(char*);
 char* int_to_binStr(uint8_t);
 int binStr_to_int(const char*);
 
@@ -22,10 +21,11 @@ void setup(void) {
 }
 
 // Update cell state
-int calculate_state(int a, int b, int c, const char* rule) {
+int calculate_state(int a, int b, int c) {
+    char* rule_str = int_to_binStr(rule_value);
     char s[4] = {a + '0', b + '0', c + '0', '\0'};
     int index = binStr_to_int(s);
-    return rule[7 - index] - '0';
+    return rule_str[7 - index] - '0';
 }
 
 // Draw one row of cells
@@ -45,16 +45,16 @@ void draw_cells(int y) {
 }
 
 // Update state for all cells
-void update_cells(const char* rule) {
+void update_cells(void) {
     // Wraparound
-    nextCells[0] = calculate_state(cells[TOTAL_CELLS -1], cells[0], cells[1], rule);
-    nextCells[TOTAL_CELLS - 1] = calculate_state(cells[TOTAL_CELLS -2], cells[TOTAL_CELLS -1], cells[0], rule);
+    nextCells[0] = calculate_state(cells[TOTAL_CELLS -1], cells[0], cells[1]);
+    nextCells[TOTAL_CELLS - 1] = calculate_state(cells[TOTAL_CELLS -2], cells[TOTAL_CELLS -1], cells[0]);
 
     for (int i = 1; i < TOTAL_CELLS - 1; i++) {
         int left = cells[i - 1];
         int right = cells[i + 1];
         int state = cells[i];
-        int newState = calculate_state(left, state, right, rule);
+        int newState = calculate_state(left, state, right);
         nextCells[i] = newState;
     }
     for (int i = 0; i < TOTAL_CELLS; i++){
@@ -82,7 +82,7 @@ int binStr_to_int(const char* str) {
     }
     return result;
 }
-void render_ca(const char* rule) {
+void render_ca(void) {
     // Window loop
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Elementary Cellular Automata");
     int y = 0;
@@ -92,7 +92,7 @@ void render_ca(const char* rule) {
         /* ClearBackground(RAYWHITE); */
         BeginDrawing();
             draw_cells(y);
-            update_cells(rule);
+            update_cells();
             // Move down 1 row
             y += CELL_WIDTH;
         EndDrawing();
@@ -100,7 +100,6 @@ void render_ca(const char* rule) {
 }
 int main(void) {
 
-    const char *rule = int_to_binStr(rule_value);
-    render_ca(rule);
+    render_ca();
     return 0;
 }
